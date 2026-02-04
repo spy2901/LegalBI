a843394fff38787009e204c6c99e0a13a3b07e1c
spy2901 <nemanja.bozovic007@gmail.com>
2026-02-04 22:33:44 +0100

    added insert for one user into database file

--- assets/database.sql
+++ assets/database.sql
@@ -11,3 +11,5 @@ CREATE TABLE IF NOT EXISTS korisnici (
     uloga ENUM('admin','analiticar','operativni') NOT NULL,
     datum_kreiranja TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO `korisnici` (`id`, `username`, `password_hash`, `uloga`, `datum_kreiranja`) VALUES (NULL, 'admin', '$2y$10$KpOPJae3eqxCN5cTs7n4xeYgYuH.ltnJXYMyiEBn.Bcxc1urvtiHG', 'admin', '2026-01-28 21:09:41')
